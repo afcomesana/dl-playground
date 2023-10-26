correct cross entropy gradient

neural-network/Layer.py
@@ -4,18 +4,28 @@ from activation import Activation, Identity
 from Unit import Unit
 
 class Layer:
-    def __init__(self, units_amount, activation = Identity):
+    def __init__(self, units_amount, activation = Identity, weights=np.array([]), bias=np.array([])):
         self.units_amount = units_amount
-        self.activation = activation
-
+        self.activation   = activation
+        self.weights      = weights
+        self.bias         = bias
+        
         if not isinstance(self.units_amount, int):
             raise Exception("Units amounts must be an intenger.")
+        
+        if units_amount < 1:
+            raise Exception("The number of units in a layer must be greater or equal to one.")
 
         if issubclass(self.activation, Activation):
             self.activation = [self.activation]*units_amount
-
-        self.units = [Unit(activation=act) for act in self.activation]
+            
+        if len(weights) == 0:
+            self.weights = np.array([np.array([]) for _ in range(self.units_amount)])
         
+        if len(self.bias) == 0:
+            self.bias = np.array([None for unit in range(self.units_amount)])
+
+        self.units = [Unit(activation=a, weights=w, bias=b) for a, w, b in zip(self.activation, self.weights, self.bias)]
         
 class InputLayer(Layer):
         

neural-network/Model.py
@@ -1,9 +1,16 @@
 import numpy as np
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+from functools import reduce
 
 from Layer import Layer, InputLayer
+from activation import Sigmoid
 from loss_functions import CrossEntropy
+
 class Model:
     def __init__(self, layers=[]):
+        self.min_loss = 1e-07
         self.layers = []
         [self.add(layer) for layer in layers]
         
@@ -42,38 +49,78 @@ class Model:
         
         return digits
     
-    def train(self, inputs, target, loss, learning_rate=0.1):
-        # Batch training:
-        if inputs.ndim > 1:
-            pass
+    def train(self, inputs, target, loss_class, learning_rate=0.1, verbose=False):
+        # TODO: Generalize special case for more-than-one-unit output layers
+
+        shortcut = False
         
-        if issubclass(loss, CrossEntropy):
+        output_activations = [unit.activation for unit in self.layers[-1].units]
+        
+        # Special case:
+        # - Loss function is cross entropy
+        # - Last layer activation function is Sigmoid
+        if issubclass(loss_class, CrossEntropy) and all(issubclass(activation, Sigmoid) for activation in output_activations):
+            
+            shortcut = True
+            
             y_hat = inputs
             for index, layer in enumerate(self.layers):
                 y_hat = layer.compute(y_hat, apply_activation=len(self.layers) != index + 1)
-                print(y_hat, len(self.layers) == index + 1)
 
         else:
             y_hat = self.predict(inputs)
+
         
-        print("Training:",loss(target, y_hat).loss, loss(target, y_hat).gradient)
-        self.backpropagation(np.matrix(loss(target, y_hat).gradient))
-        # print("Target:", target, "\nPrediction:", y_hat, "\nLoss:", loss(target, y_hat).loss, "\nGradient:",loss(target, y_hat).loss)
-        # print()
+        if len(output_activations) == 1:
+            output_activations = output_activations[0]
+            
+        loss = loss_class(target, y_hat, output_activations)
+        gradient = loss.gradient
+        loss     = loss.loss
+        
+        if loss < self.min_loss: return
+        
+        if verbose:
+            print("\n=======================================================\n")
+            print(inputs, y_hat)
+            print(loss, gradient)
+            self.info()
+            
+        self.backpropagation(np.matrix(gradient), shortcut=shortcut)
         
         [unit.update_parameters(learning_rate=learning_rate) for layer in self.layers for unit in layer.units]
+        if verbose: self.info()
         
-    def backpropagation(self, gradient):
-        # print("Initial gradient:", gradient)
+    def backpropagation(self, gradient, shortcut=False):
+        """
+        Naïve implementation of the backpropagation algorithm.
+        
+        Parameters:
+        - gradient: gradient of the loss function with respect to the output of the model.
+        - shortcut: in case a special derivative calculation has been made, this parameter must
+          be set to True, in order to not compute again the derivative of the activation function
+          in the first layer (last layer going forward)
+          
+        """
+        
         # Iterate over the layers of the model in reversed order (do not iterate over the input layer)
-        for layer in list(reversed(self.layers))[:-1]:
+        for index, layer in enumerate(list(reversed(self.layers))[:-1]):
+            
             layer_gradient = []
+            
             for index, unit in enumerate(layer.units):
+                # Incoming gradient
                 unit.gradient = gradient[:, index]
-                unit.gradient = np.sum(unit.gradient * unit.activation.gradient(unit.linear_sum))
+                
+                if not shortcut or index > 0:
+                    # Gradient of the incoming gradient with respect to the activation function of the unit
+                    unit.gradient = unit.gradient * unit.activation.gradient(unit.linear_sum)
+                
+                unit.gradient = np.sum(unit.gradient)
+                
+                # Gradient to pass on to the next layer (previous layer actually since we are going backwards):
                 layer_gradient += [unit.gradient*unit.weights]
 
-            # print("Layer gradient", layer_gradient)
             gradient = np.matrix(layer_gradient)
             
 

neural-network/Unit.py
@@ -12,7 +12,7 @@ class Unit:
     - bias: same as weights
     """
     
-    def __init__(self, activation = Identity, weights = np.array([]), bias = 0):
+    def __init__(self, activation = Identity, weights = np.array([]), bias = None):
         self.activation = activation
         self.weights    = weights
         self.bias       = bias
@@ -21,24 +21,6 @@ class Unit:
         if not issubclass(self.activation, Activation):
             raise Exception("Activation function not valid.")
             
-    def assert_inputs_type(self, inputs):
-        """
-        Check input for the unit has the proper type.
-        If the type has not a numerical value raise an error
-        """
-        
-        if isinstance(inputs, list): return True
-        if isinstance(inputs, np.ndarray): return True
-        
-        if isinstance(inputs, float):
-            inputs = np.array(inputs)
-            return True
-        
-        if isinstance(inputs, int):
-            inputs = np.array(inputs)
-            return True
-        
-        raise "Input type is not valid."
         
     def compute(self, inputs, apply_activation=True, verbose=False):
         """
@@ -48,7 +30,6 @@ class Unit:
         3. Apply activation function.
         """
         
-        self.assert_inputs_type(inputs)
         
         self.inputs = self.output = inputs
         
@@ -59,7 +40,9 @@ class Unit:
             
             self.output = np.dot(self.inputs, self.weights)
         
-        self.output += self.bias
+
+        if not self.bias is None:        
+            self.output += self.bias
         
         self.linear_sum = self.output
 
@@ -69,13 +52,19 @@ class Unit:
         return self.output
     
     def initialize_parameters(self, previous_layer_units_amount):
-        self.weights = np.random.rand(previous_layer_units_amount)
-        self.bias    = np.random.rand()
+        
+        if len(self.weights) == 0:
+            self.weights = np.random.rand(previous_layer_units_amount)
+            
+        if self.bias is None:
+            self.bias = np.random.rand()
         
     def update_parameters(self, learning_rate = 0.1):
         if self.gradient is None: return
+        
+        
+        
         # Update bias:
         self.bias -= self.gradient*learning_rate
-        
         # Update weights
-        self.weights -= np.array(self.gradient*learning_rate)
+        self.weights -= np.array(self.inputs*self.gradient*learning_rate)

neural-network/activation.py
@@ -24,11 +24,8 @@ class Activation:
         if isinstance(inputs, list) or isinstance(inputs, np.ndarray):
             output = map(lambda value: cls.compute(value), inputs)
             return list(output) if isinstance(inputs, list) else np.array(output)
-        
-        if isinstance(inputs, int) or isinstance(inputs, float):
-            return cls.compute(inputs)
-        
-        raise "Don't know how to process input."
+
+        return cls.compute(inputs)
 
 class ReLU(Activation):
     @staticmethod

neural-network/main.py
@@ -1,72 +1,84 @@
 from Layer import InputLayer, DenseLayer
 from Model import Model
+
 from loss_functions import CrossEntropy, MeanSquaredError
 
-from activation import ReLU, Activation, Sigmoid
+from activation import ReLU, Sigmoid
 import numpy as np
 import pandas as pd
-import re
-
 import matplotlib.pyplot as plt
-import seaborn as sns
-import inspect
-df = pd.read_csv("diabetes.csv")
 
-# Remove all rows with potentially missing values:
-for col in ["Pregnancies","BloodPressure","SkinThickness","Insulin","BMI","DiabetesPedigreeFunction","Age"]:
-    df = df.loc[df[col] != 0, :]
-    
-X_train = df.loc[:300, :]
-X_test  = df.loc[300:,:]
+X_train_data = np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+])
 
-X_train_data   = np.array(X_train.loc[:,X_train.columns != "Outcome"])
-X_train_target = np.array(X_train["Outcome"])
-
-X_test_data   = np.array(X_test.loc[:,X_test.columns != "Outcome"])
-X_test_target = np.array(X_test["Outcome"])
+X_train_target = np.array([0, 1, 1, 0])
 
 rows, cols = X_train_data.shape
 
 model = Model()
 model.add(InputLayer(cols))
-model.add(DenseLayer(4, ReLU))
+model.add(DenseLayer(2, ReLU))
 model.add(DenseLayer(1, Sigmoid))
 
-model.train(X_train_data[0,:], X_train_target[0], CrossEntropy)
-
-# for i in range(10):
-#     print(model.predict(X_train_data[0, :]))
-#     model.train(X_train_data[0, :], X_train_target[0], CrossEntropy)
+print("Pre-training:")
+for data, target in zip(X_train_data, X_train_target):
+    print(model.predict(data), target)
     
 
-# for epoch in range(100):
-#     for row in range(rows):
-#         model.train(X_train_data[row, :], X_train_target[row], CrossEntropy)
-        
-# for row in range(len(X_test_data)):
-#     y_hat = model.predict(X_test_data[row, :])
-#     print("Predicted: %s (%s)" % (y_hat, X_test_target[row]))
+for i in range(10000):
+    for data, target in zip(X_train_data, X_train_target):
+        model.train(data, target, CrossEntropy)
 
 
+print("Post-training:")
+for data, target in zip(X_train_data, X_train_target):
+    print(target, model.predict(data))
 
-# plt.figure(figsize=(10, 10))
-# plt.axis("scaled")
-# plt.xlim(-0.1, 1.1)
-# plt.ylim(-0.1, 1.1)
-
-# color = {
-#     0: "ro",
-#     1: "go"
-# }
-
-# for sample, target in zip(dataset["inputs"], dataset["targets"]):
-#     plt.plot(sample[0], sample[1], color[target], markersize=20)
     
-# x_range = np.arange(-0.1, 1.1, 0.01)
-# y_range = np.arange(-0.1, 1.1, 0.01)
+plt.figure(figsize=(10, 10))
+plt.xlim(-0.1, 1.1)
+plt.ylim(-0.1, 1.1)
 
-# xx, yy = np.meshgrid(x_range, y_range, indexing="ij")
-# Z = np.array([[model.predict([x, y]) for x in x_range] for y in y_range])
+color = {0: "ro",1: "go"}
 
-# plt.contourf(xx, yy, Z, levels=2, colors=["red", "green"], alpha=0.4)
-# plt.show()
+for sample, target in zip(X_train_data, X_train_target):
+    plt.plot(*sample, color[target], markersize=20)
+    
+x_range = np.arange(-0.1, 1.1, 0.01)
+y_range = np.arange(-0.1, 1.1, 0.01)
+
+# Default indexing returns the first matrix having each row as the original array
+# and the second matrix where each i-th row is the i-th element repeated as many 
+# times as elements has the first array
+
+# Indexing "ij" returns the opposite: in the first matrix, each row consist of the
+# i-th element repeated as many times as elements there are in the second array
+xx, yy = np.meshgrid(x_range, y_range, indexing="ij")
+print(model.predict(np.array([0, 1])))
+Z = np.array([[model.predict([x, y]) for x in x_range] for y in y_range])
+
+plt.contourf(xx, yy, Z, levels=2, colors=["green", "red"], alpha=0.4)
+plt.show()
+
+
+
+# df = pd.read_csv("diabetes.csv")
+
+# # Remove all rows with potentially missing values:
+# for col in ["Pregnancies","BloodPressure","SkinThickness","Insulin","BMI","DiabetesPedigreeFunction","Age"]:
+#     df = df.loc[df[col] != 0, :]
+    
+# X_train = df.loc[:300, :]
+# X_test  = df.loc[300:,:]
+
+# X_train_data   = np.array(X_train.loc[:,X_train.columns != "Outcome"])
+# X_train_target = np.array(X_train["Outcome"])
+
+# X_test_data   = np.array(X_test.loc[:,X_test.columns != "Outcome"])
+# X_test_target = np.array(X_test["Outcome"])
+
+# rows, cols = X_train_data.shape